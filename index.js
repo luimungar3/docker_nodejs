f35dea6af40b728f5eb2010efda967722e6a2f1f
luimungar3 <91976477+luimungar3@users.noreply.github.com>
2025-03-05 20:11:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');  // Para gestionar rutas de archivos
+
 var tareas = ['Tarea 1', 'Tarea 2', 'Tarea 3'];
 
+// Servir el archivo HTML cuando alguien accede a la raíz
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Ruta GET para obtener la lista de tareas
 app.get('/tareas', function (req, res) {
   res.json(tareas);
 });
 
-// Ruta para agregar una tarea
+// Ruta POST para agregar una tarea
 app.post('/tareas', express.json(), function (req, res) {
   const tarea = req.body.tarea;
   if (tarea) {
@@ -13,4 +23,12 @@ app.post('/tareas', express.json(), function (req, res) {
   } else {
     res.status(400).send('Falta la descripción de la tarea.');
   }
+});
+
+// Iniciar el servidor
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App escuchando en http://%s:%s', host, port);
 });
